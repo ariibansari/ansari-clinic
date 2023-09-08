9c2bc476a9ab756eadd1a5034008ab43194ed552
Arib Ansari <ariibansari@gmail.com>
2023-09-08 12:57:38 +0530

    lab tests send on whatsapp

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "../styles/Navbar.css"
 import { NavLink } from 'react-router-dom'
 import { IoHome, IoSettings } from "react-icons/io5"
@@ -6,8 +6,35 @@ import { FaNoteSticky } from "react-icons/fa6"
 import { RiTestTubeFill } from 'react-icons/ri'
 
 const NavigationBar = () => {
+    const [hideNav, setHideNav] = useState(false);
+
+    //automatically hides the navbar if not scrolled for 4s
+    useEffect(() => {
+        if (!hideNav) {
+            let isScrolling = setTimeout(() => {
+                if (!hideNav) {
+                    setHideNav(true)
+                    clearTimeout(isScrolling)
+                }
+            }, 4000)
+        }
+    }, [hideNav])
+
+    //shows nav bar when scrolling up and hides it when scrolling down
+    useEffect(() => {
+        window.onscroll = function (e) {
+            if (this.oldScroll > this.scrollY) {
+                setHideNav(false)
+            }
+            else {
+                setHideNav(true)
+            }
+            this.oldScroll = this.scrollY;
+        }
+    }, []);
+
     return (
-        <nav className=''>
+        <nav className={hideNav ? 'nav-hidden' : ''}>
             <NavLink to="/" className="nav-link">
                 <IoHome />
                 Home
